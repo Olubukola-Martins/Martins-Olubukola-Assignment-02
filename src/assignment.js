f7a53f04c8e34cd0ac5371cd57e3514c108ea54d
Olubukola Martins <19bukola97@gmail.com>
2022-07-12 15:22:13 +0000

    Challenge 2 and 3

--- src/assignment.js
+++ src/assignment.js
@@ -40,9 +40,6 @@ let arrayOfNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     console.log (countEvenNumbers(arrayOfNumbers))
   
 
-
-// assignment.countEvenNumbers = countEvenNumbers;
-
 /**
  * Challenge - 3
  * 
